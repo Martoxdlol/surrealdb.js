bb20a7fc315cd7f69fa791a7a6c47a2e81005cc8
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-09-21 02:46:51 +0800

    Fix broken esm declaration files

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,30 +1,41 @@
 import replace from "@rollup/plugin-replace";
 import { defineConfig } from "rollup";
-import rename from "rollup-plugin-rename";
 import typescript from "rollup-plugin-ts";
+
+const convertTsToJs = replace({
+	// by default, files are configured to work for deno, which uses exact .ts imports
+	// node, however, does not like .ts extensions and tsc will complain if we don't use .js
+	preventAssignment: true,
+	values: { ".ts": ".js" },
+});
+
+const withIsomorphicWs = replace({
+	// injects isomorphic-ws during build time for node/browser build
+	preventAssignment: true,
+	values: {
+		"// %isomorphic-ws%": 'import WebSocket from "isomorphic-ws"',
+	},
+	delimiters: ["", ""],
+});
+
+const usingTsWithEsModules = typescript({
+	hook: {
+		outputPath: (path, kind) =>
+			// replace .mts with .ts since declarations files emit without
+			// extensions and can't resolve .mts
+			kind === "declaration" ? path.replace(/\.mts$/, ".ts") : path,
+	},
+});
+
+const usingTsWithCommonJS = typescript();
 
 export default defineConfig([
 	{
 		input: "./src/index.ts",
 		plugins: [
-			replace({
-				// by default, files are configured to work for deno, which uses exact .ts imports
-				// node, however, does not like .ts extensions and tsc will complain if we don't use .js
-				preventAssignment: true,
-				values: { ".ts": ".js" },
-			}),
-			replace({
-				// injects isomorphic-ws during build time for node/browser build
-				preventAssignment: true,
-				values: {
-					"// %isomorphic-ws%":
-						'import WebSocket from "isomorphic-ws"',
-				},
-				delimiters: ["", ""],
-			}),
-			// fixes bundled tslib becoming cjs because it's inside a node_modules dir
-			rename({ map: (name) => name.replace(/node_modules/, "external") }),
-			typescript(),
+			convertTsToJs,
+			withIsomorphicWs,
+			usingTsWithEsModules,
 		],
 		external: [
 			"isomorphic-ws",
@@ -34,45 +45,34 @@ export default defineConfig([
 				dir: "dist/esm",
 				preserveModules: true,
 				preserveModulesRoot: "src",
-				plugins: [
-					// allows esm users to use named imports from our cjs package
-					rename({ map: (name) => name.replace(/\.js$/, ".mjs") }),
-				],
-			},
-			{
-				dir: "dist/cjs",
-				format: "cjs",
-				exports: "named",
-				interop: "auto",
-				preserveModules: true,
-				preserveModulesRoot: "src",
+				entryFileNames: "[name].mjs",
 			},
 		],
 	},
 	{
 		input: "./src/index.ts",
 		plugins: [
-			replace({
-				// by default, files are configured to work for deno, which uses exact .ts imports
-				// node, however, does not like .ts extensions and tsc will complain if we don't use .js
-				preventAssignment: true,
-				values: { ".ts": ".js" },
-			}),
-			typescript({
-				tsconfig: {
-					strict: true,
-					alwaysStrict: true,
-					skipLibCheck: true,
-					isolatedModules: true,
-					forceConsistentCasingInFileNames: true,
-					target: "ES2020",
-					module: "ES6",
-					lib: [
-						"DOM",
-						"ESNext",
-					],
-				},
-			}),
+			convertTsToJs,
+			withIsomorphicWs,
+			usingTsWithCommonJS,
+		],
+		external: [
+			"isomorphic-ws",
+		],
+		output: {
+			dir: "dist/cjs",
+			format: "cjs",
+			exports: "named",
+			interop: "auto",
+			preserveModules: true,
+			preserveModulesRoot: "src",
+		},
+	},
+	{
+		input: "./src/index.ts",
+		plugins: [
+			convertTsToJs,
+			usingTsWithEsModules,
 		],
 		output: {
 			dir: "./dist/web",
